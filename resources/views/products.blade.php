acc17db5cb682d23f40f763e3f4f13798706eb83
Taris Henrique <taris.tj@hotmail.com>
2022-09-06 16:29:00 -0300

    Aula 08 - Resgatando parametros de URL

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Início')
+@section('content')
+
 <h1> Tela de Produtos </h1>
+
+@if ($busca != '')
+    <p>O usuário está buscando por: {{ $busca }} </p>
+    @endif
+
+@endsection
+
