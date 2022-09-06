1f003f4043a92b41cfc222e327e1651d188413e2
Taris Henrique <taris.tj@hotmail.com>
2022-09-06 14:34:40 -0300

    Aula 04 - Blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Taris";
+    $idade = 29;
+
+    return view('welcome', ['nome' => $nome, 'idade2' => $idade, 'profissao' => "Programador"]);
 });
 
 Route::get('/contatos', function () {
