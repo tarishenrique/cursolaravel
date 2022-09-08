bec79bb63d256bfa460e015fe11a760259141a3d
Taris Henrique <taris.tj@hotmail.com>
2022-09-08 15:05:17 -0300

    08-09-2022

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,23 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+
 class EventController extends Controller
 {
     public function index()
     {
-        $nome = "Taris";
-        $idade = 29;
+        $events = Event::all();
 
-        $arr = [1, 2, 3, 4, 5];
-        $nomes = ["Taris", "Maria", "João", "Saulo"];
-
-        return view('welcome', [
-            'nome' => $nome,
-            'idade2' => $idade,
-            'profissao' => "Programador",
-            'arr' => $arr,
-            'nomes' => $nomes
-        ]);
+        return view('welcome', ['events' => $events]);
     }
 
     public function create()
