800828cd33e5b98980290f85dd7732219d04f15a
Taris Henrique <taris.tj@hotmail.com>
2022-09-08 16:47:51 -0300

    Aula 16 - Flash Messages

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -44,7 +44,17 @@
 
     </header>
 
-    @yield('content')
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                <p class="msg"> {{ session('msg') }} </p>
+                @endif
+                @yield('content')
+            </div>
+        </div>
+
+    </main>
 
     <footer>
         <p>Taris Henrique &copy; 2022</p>
